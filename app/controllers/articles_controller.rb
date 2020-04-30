055a3492b04226b8103d260a90f827a48aebe85c
LogaSangeerani <logasivanantham@gmail.com>
2020-04-30 12:55:24 +0530

    Basic Working template with clean code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        # byebug
-        @article = Article.find(params[:id])
+        
     end
 
     def index
@@ -12,8 +13,12 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        
+    end
+
     def create
-        @article = Article.new(params.require(:article).permit(:title,:description))  
+        @article = Article.new(article_params)  
 
         if @article.save  
             flash[:notice] = "Article was created successfully"  
@@ -22,4 +27,29 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully" 
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+private 
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title,:description)
+    end
+    
 end
