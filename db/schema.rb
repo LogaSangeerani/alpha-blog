6689c95c365b10334133b0bef7c87415de41b4ed
LogaSangeerani <logasivanantham@gmail.com>
2020-05-07 18:32:10 +0530

    Timestamp change and add username from table in index and show view

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,8 @@ ActiveRecord::Schema.define(version: 2020_05_07_065451) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.time "created_at"
-    t.time "updated_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
   end
 
