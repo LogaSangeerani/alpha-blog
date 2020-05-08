db0f7ff0956fa5930a8309485f444289811e621c
LogaSangeerani <logasivanantham@gmail.com>
2020-05-08 12:15:40 +0530

    New User Signup form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles
-  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  resources :articles                # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]                                  # post 'users', to: 'users#create' only one. resources will have all routes
 end
