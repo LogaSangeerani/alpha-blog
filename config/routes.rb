217109c0173c2bbc85f772bc7a370efe1490b986
LogaSangeerani <logasivanantham@gmail.com>
2020-05-11 13:48:29 +0530

    User login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :articles                # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   get 'signup', to: 'users#new'
   resources :users, except: [:new]                                  # post 'users', to: 'users#create' only one. resources will have all routes
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  post 'logout', to: 'sessions#destroy'
 end
